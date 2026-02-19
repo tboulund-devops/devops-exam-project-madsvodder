1e2c227dc0f482bef92f492fc203c76fb046b3b6
madsvodder <madsvodder@gmail.com>
2026-02-19 14:35:58 +0100

    added test project

---
+++ Backend/FreshTomatoes.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace FreshTomatoes.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        int a = 2;
+        int b = 3;
+
+        // Act
+        int result = a + b;
+
+        // Assert
+        Assert.Equal(5, result);
+    }
+}
