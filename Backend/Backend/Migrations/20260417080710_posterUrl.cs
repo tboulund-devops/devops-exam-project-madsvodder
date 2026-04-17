ad6da8626492d1da55867dccdbec6aac2e623453
HrHolm1 <robinholm123456789@gmail.com>
2026-04-17 10:07:55 +0200

    Migrations

---
+++ Backend/Backend/Migrations/20260417080710_posterUrl.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class posterUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PosterUrl",
+                table: "Movies",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PosterUrl",
+                table: "Movies");
+        }
+    }
+}
